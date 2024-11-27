4320f1ad63d9cabf837afeacc1c5ed2386fb4be8
gsizer <gsizer@gmail.com>
2024-11-27 08:35:09 -0700

    Initial commit

---
+++ Framework.mm
@@ -0,0 +1,144 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Framework" FOLDED="false" ID="ID_928167138" CREATED="1732719855325" MODIFIED="1732721106410" STYLE="oval"><hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.connection" LAST="false">
+            <node_periodic_level_condition PERIOD="2" REMAINDER="1"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.topic" LAST="false">
+            <node_level_condition VALUE="2" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subtopic" LAST="false">
+            <node_level_condition VALUE="4" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subsubtopic" LAST="false">
+            <node_level_condition VALUE="6" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/conceptMap.mm" fit_to_viewport="false" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_187729897" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_187729897" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Arial" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="8" BOLD="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Game Data" POSITION="bottom_or_right" ID="ID_850938071" CREATED="1732719913534" MODIFIED="1732719922578">
+<node TEXT="World" ID="ID_1010953531" CREATED="1732720103070" MODIFIED="1732720127678">
+<node TEXT="Area / Zone" ID="ID_11478511" CREATED="1732720236472" MODIFIED="1732720281125">
+<node TEXT="Size" ID="ID_506936216" CREATED="1732720727300" MODIFIED="1732720729392"/>
+<node TEXT="Tasks" ID="ID_550705874" CREATED="1732720729676" MODIFIED="1732720743837">
+<node TEXT="Position Start/End" ID="ID_1764470531" CREATED="1732720885758" MODIFIED="1732720908935"/>
+<node TEXT="Objective" ID="ID_600330251" CREATED="1732720909230" MODIFIED="1732720922915">
+<node TEXT="move to" ID="ID_1251906832" CREATED="1732720922917" MODIFIED="1732720954787"/>
+<node TEXT="move object" ID="ID_1069404367" CREATED="1732720934800" MODIFIED="1732720973024"/>
+<node TEXT="destroy target" ID="ID_1928415614" CREATED="1732720956696" MODIFIED="1732720962543"/>
+<node TEXT="protect target" ID="ID_28375686" CREATED="1732720963723" MODIFIED="1732720968173"/>
+</node>
+<node TEXT="Award" ID="ID_628549097" CREATED="1732720988423" MODIFIED="1732721000759">
+<node TEXT="Success" ID="ID_841102158" CREATED="1732721001423" MODIFIED="1732721003544"/>
+<node TEXT="Failure" ID="ID_827451246" CREATED="1732721003973" MODIFIED="1732721006840"/>
+</node>
+</node>
+<node TEXT="NPCs" ID="ID_271215885" CREATED="1732720317286" MODIFIED="1732720880399">
+<node TEXT="Statis" ID="ID_1436151459" CREATED="1732720577499" MODIFIED="1732720613356"/>
+<node TEXT="Position" ID="ID_335760737" CREATED="1732720616336" MODIFIED="1732720624911"/>
+<node TEXT="Health" ID="ID_1820137442" CREATED="1732720625176" MODIFIED="1732720628276"/>
+<node TEXT="Hunger" ID="ID_707737986" CREATED="1732720628551" MODIFIED="1732720630787"/>
+<node TEXT="Thirst" ID="ID_576925870" CREATED="1732720631051" MODIFIED="1732720637132"/>
+<node TEXT="Stamina" ID="ID_280980614" CREATED="1732720637351" MODIFIED="1732720640253"/>
+<node TEXT="Gaes" ID="ID_1321441461" CREATED="1732720640511" MODIFIED="1732720643032"/>
+</node>
+<node TEXT="Static Objects" ID="ID_1288225154" CREATED="1732720750001" MODIFIED="1732720793729"/>
+<node TEXT="Dynamic Objects" ID="ID_1193838623" CREATED="1732720824310" MODIFIED="1732720828986"/>
+</node>
+</node>
+<node TEXT="Player" ID="ID_1073948302" CREATED="1732719969709" MODIFIED="1732719980466">
+<node TEXT="Character" ID="ID_1467470303" CREATED="1732720133522" MODIFIED="1732720137398">
+<node TEXT="Statistics" ID="ID_282282618" CREATED="1732720328902" MODIFIED="1732720337998"/>
+<node TEXT="Position" ID="ID_684251391" CREATED="1732720360387" MODIFIED="1732720363928"/>
+<node TEXT="Health" ID="ID_1571000911" CREATED="1732720364703" MODIFIED="1732720382110"/>
+<node TEXT="Hunger" ID="ID_1793659928" CREATED="1732720382564" MODIFIED="1732720401276"/>
+<node TEXT="Thirst" ID="ID_120312226" CREATED="1732720401650" MODIFIED="1732720405036"/>
+<node TEXT="Stamina" ID="ID_365406029" CREATED="1732720408945" MODIFIED="1732720412926"/>
+<node TEXT="Gaes" ID="ID_1763991207" CREATED="1732720413290" MODIFIED="1732720419047"/>
+</node>
+<node TEXT="Preferences" ID="ID_232286086" CREATED="1732720138068" MODIFIED="1732720143444">
+<node TEXT="Key Bindings" ID="ID_1286162819" CREATED="1732720660751" MODIFIED="1732720664939"/>
+<node TEXT="Interface Scaling" ID="ID_1464547465" CREATED="1732720666623" MODIFIED="1732720673264"/>
+<node TEXT="Assistive Technologies" ID="ID_809575112" CREATED="1732720673529" MODIFIED="1732720684395"/>
+</node>
+</node>
+</node>
+<node TEXT="User Interface" POSITION="bottom_or_right" ID="ID_839367407" CREATED="1732719896712" MODIFIED="1732719910912">
+<node TEXT="Menu Design" ID="ID_537038993" CREATED="1732720044752" MODIFIED="1732720057664">
+<node TEXT="Layout" ID="ID_246060886" CREATED="1732720150638" MODIFIED="1732720153093"/>
+<node TEXT="Theme" ID="ID_1969901244" CREATED="1732720154008" MODIFIED="1732720157433"/>
+</node>
+<node TEXT="Configuration Options" ID="ID_292325740" CREATED="1732720034823" MODIFIED="1732720040969">
+<node TEXT="Audio" ID="ID_1878663041" CREATED="1732720165678" MODIFIED="1732720168264"/>
+<node TEXT="Graphics" ID="ID_1263816969" CREATED="1732720169009" MODIFIED="1732720171755"/>
+<node TEXT="Game" ID="ID_1844880492" CREATED="1732720174589" MODIFIED="1732720183280"/>
+</node>
+<node TEXT="File Management" ID="ID_103962072" CREATED="1732720063248" MODIFIED="1732720077915">
+<node TEXT="Create" ID="ID_1898286186" CREATED="1732720187569" MODIFIED="1732720195275"/>
+<node TEXT="Save" ID="ID_1599592945" CREATED="1732720195944" MODIFIED="1732720201416"/>
+<node TEXT="Load" ID="ID_716143103" CREATED="1732720201990" MODIFIED="1732720204531"/>
+<node TEXT="Erase" ID="ID_839220317" CREATED="1732720205395" MODIFIED="1732720213902"/>
+</node>
+</node>
+</node>
+</map>
---
+++ README.md
@@ -0,0 +1,2 @@
+# framework
+ Framework is an open and free concept map document that is convertible to any development environment for modification and reuse.
